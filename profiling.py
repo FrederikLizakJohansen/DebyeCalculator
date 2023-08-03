cda0d651e9b367264bcfc2cb8cc0fcd02b667747
Frederik <frjo@di.ku.dk>
2023-08-03 08:58:09 +0200

    first commit

---
+++ profiling.py
@@ -0,0 +1,58 @@
+import collections
+import timeit
+
+
+class Timings:
+
+    def __init__(self):
+        self._means = collections.defaultdict(int)
+        self._vars = collections.defaultdict(int)
+        self._counts = collections.defaultdict(int)
+        self.reset()
+
+    def reset(self):
+        self.last_time = timeit.default_timer()
+
+    def time(self, name):
+        now = timeit.default_timer()
+        x = now - self.last_time
+        self.last_time = now
+
+        n = self._counts[name]
+
+        mean = self._means[name] + (x - self._means[name]) / (n + 1)
+        var = (
+            n * self._vars[name] + n * (self._means[name] - mean) ** 2 + (x - mean) ** 2
+        ) / (n + 1)
+
+        self._means[name] = mean
+        self._vars[name] = var
+        self._counts[name] += 1
+
+    def means(self):
+        return self._means
+
+    def vars(self):
+        return self._vars
+
+    def stds(self):
+        return {k: v ** 0.5 for k, v in self._vars.items()}
+
+    def total(self):
+        return sum(self.means().values())
+
+    def summary(self, prefix=""):
+        means = self.means()
+        stds = self.stds()
+        total = sum(means.values())
+
+        result = prefix
+        for k in sorted(means, key=means.get, reverse=True):
+            result += f"\n   -> %s: %.3fms +- %.3fms (%.2f%%) " % (
+                k,
+                1000 * means[k],
+                1000 * stds[k],
+                100 * means[k] / total,
+            )
+        result += "\nTotal: %.3fms" % (1000 * total)
+        return result
