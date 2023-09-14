5731d001102122ba031b331b74312e14b850c5bd
Frederik <frjo@di.ku.dk>
2023-09-14 17:52:32 +0200

    Simplified profler

--- DebyeCalculator/profiling.py
+++ DebyeCalculator/profiling.py
@@ -4,33 +4,17 @@ import timeit
 class Profiler:
     """
     Profiler
-
     This class provides a simple profiling mechanism for measuring the execution time of different sections of code. It records the time taken for each named section of code and calculates the mean and variance of the recorded times.
 
     Methods:
-        __init__():
-            Initialize the Profiler object with default settings.
-
-        reset():
-            Reset the profiler data and start tracking time from the current point.
-
-        time(name):
-            Record the execution time for a specific section of code with the given name.
-
-        means():
-            Get the dictionary of mean times for each recorded section.
-
-        vars():
-            Get the dictionary of variances of the recorded times for each section.
-
-        stds():
-            Get the dictionary of standard deviations of the recorded times for each section.
-
-        total():
-            Calculate the total time taken for all recorded sections.
-
-        summary(prefix=""):
-            Generate a summary of the profiling data with mean time, standard deviation, and percentage of total time for each recorded section.
+        __init__(): Initialize the Profiler object with default settings.
+        reset(): Reset the profiler data and start tracking time from the current point.
+        time(name): Record the execution time for a specific section of code with the given name.
+        means(): Get the dictionary of mean times for each recorded section.
+        vars(): Get the dictionary of variances of the recorded times for each section.
+        stds(): Get the dictionary of standard deviations of the recorded times for each section.
+        total(): Calculate the total time taken for all recorded sections.
+        summary(prefix=""): Generate a summary of the profiling data with mean time, standard deviation, and percentage of total time for each recorded section.
 
     Usage:
         The Profiler object can be used to measure the execution time of different parts of a code by calling the `time` method with a descriptive name for each section. After profiling, the `summary` method can be used to print a summary of the profiling data.
