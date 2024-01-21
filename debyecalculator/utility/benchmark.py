e928d2a19dce6b3b374f61cc4075a99f954837b9
Frederik <frjo@di.ku.dk>
2024-01-21 13:42:52 +0100

    simplified benchmarker class

--- debyecalculator/utility/benchmark.py
+++ debyecalculator/utility/benchmark.py
@@ -38,7 +38,8 @@ class Statistics:
         name: str,
         radii: List[float],
         num_atoms: List[int],
-        cuda_mem: List[float],
+        cuda_mem_structure: List[float],
+        cuda_mem_calculations: List[float],
         device: str,
         batch_size: int
     ) -> None:
@@ -61,17 +62,18 @@ class Statistics:
         self.name = name
         self.radii = radii
         self.num_atoms = num_atoms
-        self.cuda_mem = cuda_mem
+        self.cuda_mem_structure = cuda_mem_structure
+        self.cuda_mem_calculations = cuda_mem_calculations
         self.device = device
         self.batch_size = batch_size
         
         # Create table
-        self.table_fields = ['Radius [Å]', 'Num. atoms', 'Mean [s]', 'Std [s]', 'MaxAlloc. CUDA mem [MB]']
+        self.table_fields = ['Radius [Å]', 'Num. atoms', 'Mean [s]', 'Std [s]', 'MaxAlloc. CUDA Memory (Structure) [MB]', 'MaxAlloc. CUDA Memory (Calc) [MB]']
         self.pt = PrettyTable(self.table_fields)
         self.pt.align = 'r'
         self.pt.padding_width = 1
         self.pt.title = 'Benchmark Generator / ' + self.device + ' / Batch Size: ' + str(self.batch_size)
-        self.data = [[str(float(r)), str(int(n)), f'{m:1.5f}', f'{s:1.5f}', f'{c:1.5f}'] for r,n,m,s,c in zip(self.radii, list(num_atoms), list(means), list(stds), list(cuda_mem))]
+        self.data = [[str(float(r)), str(int(n)), f'{m:1.5f}', f'{s:1.5f}', f'{cs:1.5f}', f'{cc:1.5f}'] for r,n,m,s,cs,cc in zip(self.radii, list(num_atoms), list(means), list(stds), list(cuda_mem_structure), list(cuda_mem_calculations))]
         for d in self.data:
             self.pt.add_row(d)
 
@@ -85,7 +87,7 @@ class Statistics:
         """
         Return a detailed string representation of the Statistics object.
         """
-        return f'Statistics (\n\tname = {self.name},\n\tradii = {self.radii},\n\tnum_atoms = {self.num_atoms},\n\tmeans = {self.means},\n\tstds = {self.stds},\n\tcuda_mem = {self.cuda_mem},\n\tbatch_size = {self.batch_size},\n\tdevice = {self.device}\n)'
+        return f'Statistics (\n\tname = {self.name},\n\tradii = {self.radii},\n\tnum_atoms = {self.num_atoms},\n\tmeans = {self.means},\n\tstds = {self.stds},\n\tcuda_mem_structure = {self.cuda_mem_structure},\n\tcuda_mem_calculations = {self.cuda_mem_calculations},\n\tbatch_size = {self.batch_size},\n\tdevice = {self.device}\n)'
 
 class DebyeBenchmarker:
     """
@@ -100,6 +102,7 @@ class DebyeBenchmarker:
     """
     def __init__(
         self,
+        function: str = 'gr',
         radii: Union[List, np.ndarray, torch.Tensor] = [5],
         show_progress_bar: bool = True,
         custom_cif: str = None,
@@ -118,11 +121,21 @@ class DebyeBenchmarker:
         self.set_radii(list(radii))
         self.cif = pkg_resources.resource_filename(__name__, 'benchmark_structure.cif')
         self.custom_cif = custom_cif
-
-        self.show_progress_bar = show_progress_bar
+        
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             self.debye_calc = DebyeCalculator(**kwargs)
+            
+        if function == 'gr':
+            self.func = self.debye_calc.gr
+        elif function == 'iq':
+            self.func = self.debye_calc.iq
+        elif function == 'sq':
+            self.func = self.debye_calc.sq
+        else:
+            raise ValueError("Invalid value for 'function', please provide either 'gr', 'iq' or 'sq'")
+
+        self.show_progress_bar = show_progress_bar
 
     def set_debye_parameters(self, **debye_parameters: Any) -> None:
         """
@@ -160,145 +173,94 @@ class DebyeBenchmarker:
         """
         self.radii = list(radii)
     
-    def benchmark_generation(
+    def benchmark(
         self,
-        individually: bool = False,
+        generate_individually: bool = True,
         repetitions: int = 1,
     ) -> Statistics:
         """
-        Benchmark nanoparticle generation.
+        Benchmark DebyeCalculator.
 
         Parameters:
-        - individually (bool): Flag to benchmark individually for each radius.
+        - generate_individually (bool): Flag to benchmark individually for each radius.
         - repetitions (int): Number of repetitions for benchmarking.
 
         Returns:
         - Statistics: Benchmark statistics.
         """
         
+        # Ignore warnings concerning CUDA availability
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
 
-            # Create metrics arrays
-            means = np.zeros(len(self.radii))
-            stds = np.zeros(len(self.radii))
-            num_atoms = np.zeros(len(self.radii))
-            cuda_mem = np.zeros(len(self.radii))
-            
-            cif_file = self.custom_cif if self.custom_cif is not None else self.cif
-            name = cif_file.split('/')[-1]
-            
-            if not individually:
-                times = []
-                mems = []
-                if self.debye_calc.device == 'cuda':
-                    torch.cuda.reset_max_memory_allocated()
-                for j in range(repetitions + 2):
-                    t = time()
-                    nanoparticles = generate_nanoparticles(cif_file, self.radii, _reverse_order=False, disable_pbar = True, device=self.debye_calc.device)
-                    t = time() - t
-                    if j > 1:
-                        times.append(t)
-
-                    if self.debye_calc.device == 'cuda':
-                        mems.append(torch.cuda.max_memory_allocated() / 1_000_000)
-                    else:
-                        mems.append(0)
-
-                # Collect metrics
-                means[:] = np.mean(times) / len(self.radii)
-                stds[:] = np.std(times) / len(self.radii)
-                num_atoms = [n.size for n in nanoparticles]
-                cuda_mem[:] = np.mean(mems)
-            else:
-                pbar = tqdm(desc='Benchmarking Nanoparticle Generation...', total=len(self.radii), disable = not self.show_progress_bar)
-                for i in range(len(self.radii)):
-                    times = []
-                    mems = []
-                    if self.debye_calc.device == 'cuda':
-                        torch.cuda.reset_max_memory_allocated()
-                    for j in range(repetitions + 2):
-                        t = time()
-                        nanoparticles = generate_nanoparticles(cif_file, [self.radii[i]], _reverse_order=False, disable_pbar = True, device=self.debye_calc.device)
-                        t = time() - t
-                        if j > 1:
-                            times.append(t)
-
-                        if self.debye_calc.device == 'cuda':
-                            mems.append(torch.cuda.max_memory_allocated() / 1_000_000)
-                        else:
-                            mems.append(0)
-
-                    # Collect metrics
-                    means[i] = np.mean(times)
-                    stds[i] = np.std(times)
-                    num_atoms[i] = nanoparticles[0].size
-                    cuda_mem[i] = np.mean(mems)
-                    
-                    pbar.update(1)
-                pbar.close()
-        
-        return Statistics(list(means), list(stds), name, self.radii, list(num_atoms), list(cuda_mem), self.debye_calc.device, self.debye_calc.batch_size)
-
-    def benchmark_calculator(
-        self,
-        repetitions: int = 1,
-    ) -> Statistics:
-        """
-        Benchmark Debye calculator.
-
-        Parameters:
-        - repetitions (int): Number of repetitions for benchmarking.
-        - csv_output (Union[None, str]): Path to save CSV output (if provided).
-
-        Returns:
-        - Statistics: Benchmark statistics.
-        """
-        
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
+            # Using CUDA?
+            on_cuda = self.debye_calc.device == 'cuda'
              
             # Create metrics arrays
             means = np.zeros(len(self.radii))
             stds = np.zeros(len(self.radii))
             num_atoms = np.zeros(len(self.radii))
-            cuda_mem = np.zeros(len(self.radii))
+            cuda_mem_structure = np.zeros(len(self.radii))
+            cuda_mem_calculations = np.zeros(len(self.radii))
 
             # Create nanoparticles seperate, such that exact metrics can be extracted
             cif_file = self.custom_cif if self.custom_cif is not None else self.cif
             name = cif_file.split('/')[-1]
-            nanoparticles = generate_nanoparticles(cif_file, self.radii, _reverse_order=False, disable_pbar = True, device=self.debye_calc.device)
+            
+            # Iterator
+            if generate_individually:
+                nanoparticles = lambda i: generate_nanoparticles(cif_file, self.radii[i], _reverse_order=False, disable_pbar = True, device = self.debye_calc.device)
+            else:
+                if on_cuda: torch.cuda.reset_max_memory_allocated()
+                nanoparticles = generate_nanoparticles(cif_file, self.radii, _reverse_order=False, disable_pbar = True, device=self.debye_calc.device)
+                mean_cuda_mem_structure = torch.cuda.max_memory_allocated() / 1_000_000 if on_cuda else 0
 
             # Benchmark
             pbar = tqdm(desc='Benchmarking Calculator...', total=len(self.radii), disable = not self.show_progress_bar)
-            for i,nano in enumerate(nanoparticles):
-                times = []
-                mems = []
-                if self.debye_calc.device == 'cuda':
-                    torch.cuda.reset_max_memory_allocated()
-                for j in range(repetitions+2):
+            for i in range(len(self.radii)):
 
+                # Reset memory allocation count
+                if on_cuda: torch.cuda.reset_max_memory_allocated()
+                    
+                # Fetch nanoparticle
+                nano = nanoparticles(i)[0] if generate_individually else nanoparticles[i]
+
+                # Collect memory allocation
+                if generate_individually:
+                    cuda_mem_structure[i] = torch.cuda.max_memory_allocated() / 1_000_000 if on_cuda else 0
+                else:
+                    cuda_mem_structure[i] = mean_cuda_mem_structure
+
+                # Lists
+                times = []
+                mems_calculations = []
+                for j in range(repetitions+2):
+                
+                    # Reset allocation
+                    if on_cuda: torch.cuda.reset_max_memory_allocated()
+                    
+                    # Time calculation of gr (encompases all possible calculations of DebyeCalculator)
                     t = time()
-                    data = self.debye_calc.gr((nano.elements, nano.xyz))
+                    data = self.func((nano.elements, nano.xyz))
                     t = time() - t
+
+                    # Append only after dummy repetitions
                     if j > 1:
                         times.append(t)
 
-                    if self.debye_calc.device == 'cuda':
-                        mems.append(torch.cuda.max_memory_allocated() / 1_000_000)
-                    else:
-                        mems.append(0)
+                    # Append memory allocation
+                    mems_calculations.append(torch.cuda.max_memory_allocated() / 1_000_000) if on_cuda else mems_calculations.append(0)
 
                 # Collect metrics
                 means[i] = np.mean(times)
                 stds[i] = np.std(times)
                 num_atoms[i] = nano.size
-                cuda_mem[i] = np.mean(mems)
+                cuda_mem_calculations[i] = np.mean(mems_calculations)
 
                 pbar.update(1)
             pbar.close()
 
-        return Statistics(list(means), list(stds), name, self.radii, list(num_atoms), list(cuda_mem), self.debye_calc.device, self.debye_calc.batch_size)
+        return Statistics(list(means), list(stds), name, self.radii, list(num_atoms), list(cuda_mem_structure), list(cuda_mem_calculations), self.debye_calc.device, self.debye_calc.batch_size)
 
 def to_csv(stat: Statistics, path: str) -> None:
     """
@@ -353,11 +315,13 @@ def from_csv(path: str) -> Statistics:
     num_atoms = []
     means = []
     stds = []
-    cuda_mem = []
+    cuda_mem_structure = []
+    cuda_mem_calculations = []
     for row in csv_reader:
         radii.append(float(row[0]))
         num_atoms.append(int(row[1]))
         means.append(float(row[2]))
         stds.append(float(row[3]))
-        cuda_mem.append(float(row[4]))
-    return Statistics(means, stds, name, radii, num_atoms, cuda_mem, device, batch_size)
+        cuda_mem_structure.append(float(row[4]))
+        cuda_mem_calculations.append(float(row[5]))
+    return Statistics(means, stds, name, radii, num_atoms, cuda_mem_structure, cuda_mem_calculations, device, batch_size)
