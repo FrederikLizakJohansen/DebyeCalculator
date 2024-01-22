dcf80d4c7a8e7e2b0f43b98467d1a600a40b516c
Andy S. Anker <ansoan@dtu.dk>
2024-01-22 08:27:30 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,6 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-autodoc_mock_imports = ["torch", "debyecalculator"]
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
